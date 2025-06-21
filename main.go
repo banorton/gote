f8bd1b63597e5e3d4ec5e87eaaa924cef7edeb4b
banorton <banorton1@gmail.com>
2025-06-20 23:04:52 -0400

    added util for opening in editor. started on gote note

--- main.go
+++ main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
+	"path/filepath"
 )
 
 func main() {
 	args := os.Args
 
 	if len(args) == 1 {
-		note()
+		note(nil)
 		return
 	}
 
@@ -27,12 +29,51 @@ func main() {
 	case "config":
 		config(args[2:])
 	default:
-		note()
+		note(args[1:])
 	}
 }
 
-func note() {
+func note(args []string) {
+	if len(args) < 1 {
+		fmt.Println("Usage: gote <note-name>")
+		return
+	}
 
+	noteName := args[0]
+	cfg, err := loadConfig()
+	if err != nil {
+		fmt.Println("Error loading config:", err)
+		return
+	}
+
+	noteDir := cfg.NoteDir
+	if noteDir == "" {
+		noteDir = defaultConfig().NoteDir
+	}
+
+	if err := os.MkdirAll(noteDir, 0755); err != nil {
+		fmt.Println("Error creating notes directory:", err)
+		return
+	}
+
+	notePath := filepath.Join(noteDir, noteName+".md")
+	if _, err := os.Stat(notePath); os.IsNotExist(err) {
+		f, err := os.Create(notePath)
+		if err != nil {
+			fmt.Println("Error creating note:", err)
+			return
+		}
+		f.Close()
+	}
+
+	editor := cfg.Editor
+	if editor == "" {
+		fmt.Println("No editor specified in config.")
+	}
+
+	if err := openFileInEditor(editor, notePath), err != nil {
+		fmt.Println(err)
+	}
 }
 
 func index() {
